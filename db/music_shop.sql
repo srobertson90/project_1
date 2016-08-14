616130323a4c28f737636ecd48bc50f0177e1160
srobertson90 <stuart.robertson.applications@gmail.com>
2016-08-14 18:32:48 +0100

    change variable types for stocks

--- db/music_shop.sql
+++ db/music_shop.sql
@@ -3,23 +3,23 @@ DROP TABLE artists;
 DROP TABLE stocks;
 
 CREATE TABLE artists (
-  id serial4 primary key,
-  name varchar(255),
-  genre varchar(255)
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255),
+  genre VARCHAR(255)
 );
 
 CREATE TABLE albums (
-  id serial4 primary key,
-  name varchar(255),
-  artist_id int4 references artists(id) on delete cascade
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255),
+  artist_id int4 REFERENCES artists(id) ON DELETE CASCADE
 );
 
 CREATE TABLE stocks (
-  id serial4 primary key,
-  album_id int4 references albums(id) on delete cascade,
-  format varchar(255),
-  stock_level integer,
-  threshold integer,
-  buy_price money,
-  sell_price money
+  id serial4 PRIMARY KEY,
+  album_id int4 REFERENCES albums(id) ON DELETE CASCADE,
+  format VARCHAR(255),
+  stock_level INT4,
+  threshold INT4,
+  buy_price INT4,
+  sell_price INT4
 )
